f4dc1d77177dcec5d7d9441a03efb66cc972471c
matrickpf21 <patrickfigueroa23@gmail.com>
2024-01-31 06:18:48 -0500

    chore: use prompts instead of cli arguments

---
+++ _templates/modulo/new/prompt.js
@@ -0,0 +1,10 @@
+/** 
+ * @type {import("../../controller/new/prompt").Prompt[]}
+ */
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Nombre del modulo (ex. NuevoModulo)"
+  },
+]
