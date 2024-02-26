1e54acca59793cb12c152257b396c3d2dd5a08f0
matrickpf21 <patrickfigueroa23@gmail.com>
2024-02-26 11:04:13 -0500

    fix: broken mock

--- src/Tests/Unit/__mocks__/AsignaturaEnVarianteCursoRepository.ts
+++ src/Tests/Unit/__mocks__/AsignaturaEnVarianteCursoRepository.ts
@@ -2,13 +2,27 @@ import * as crypto from "crypto";
 import { injectable } from "inversify";
 
 import type { IAsignaturaEnVarianteCurso } from "../../../Core/AsignaturaEnVarianteCurso/Domain/IAsignaturaEnVarianteCurso";
-import type { IAsignaturaEnVarianteCursoRepository } from "../../../Core/AsignaturaEnVarianteCurso/Domain/IAsignaturaEnVarianteCursoRepository";
+import type {
+	IAsignaturaEnVarianteCursoRepository,
+	UpdateAsignaturaEnVarianteCursoParams,
+} from "../../../Core/AsignaturaEnVarianteCurso/Domain/IAsignaturaEnVarianteCursoRepository";
 import type { ICreateAsignaturaEnVarianteCurso } from "../../../Core/AsignaturaEnVarianteCurso/Domain/ICreateAsignaturaEnVarianteCurso";
 
 @injectable()
 export class MockAsignaturaEnVarianteCursoRepository
 	implements IAsignaturaEnVarianteCursoRepository
 {
+	deleteById(_: string): Promise<IAsignaturaEnVarianteCurso> {
+		return Promise.resolve({} as IAsignaturaEnVarianteCurso);
+	}
+	getById(_: string): Promise<IAsignaturaEnVarianteCurso | null> {
+		return Promise.resolve({} as IAsignaturaEnVarianteCurso);
+	}
+	update(
+		_: UpdateAsignaturaEnVarianteCursoParams,
+	): Promise<IAsignaturaEnVarianteCurso> {
+		return Promise.resolve({} as IAsignaturaEnVarianteCurso);
+	}
 	async create({
 		asignaturaId,
 		varianteCursoId,
